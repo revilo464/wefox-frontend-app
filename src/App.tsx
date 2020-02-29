114afba6c00841b3fe720b807e6081b4daa52fef
user <oli@revilo.com>
2020-02-29 16:46:11 +0100

    Listing posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import PropTypes from "prop-types";
+// import logo from './logo.svg';
 import './App.css';
+
+interface Post {
+  id: number,
+  title: string,
+  content: string,
+  lat: number,
+  long: number,
+  image_url: string,
+  created_at: Date,
+  updated_at: Date,
+}
+
+const Posts = () => {
+  const [hasErrors, setErrors] = useState(false);
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  async function fetchData() {
+    const res = await fetch("https://wf-challenge-d6haqugtoo.herokuapp.com/api/v1/posts");
+    res.json()
+      .then(res => setPosts(res))
+      .catch(err => setErrors(err));
+  }
+
+  useEffect(() => {
+    fetchData();
+  });
+
+  const post_list = posts.map((item, key) =>
+    <li>{item.title}</li>
+  );
+
+  return (
+    <div className="container">
+      {post_list}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ol>
+          <Posts/>
+        </ol>
       </header>
     </div>
   );
